1539f8c9792e3f85e4cbdcd458ffec2bf9a11c74
nayira <114664058+vd8761@users.noreply.github.com>
2026-05-20 15:27:52 +0530

    Update Enhancement

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://askastroraja.com',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
