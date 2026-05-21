54a3fb4058ca39678b469837476a6c23e36a8389
nayira <114664058+vd8761@users.noreply.github.com>
2026-05-21 17:26:07 +0530

    Updated Contexts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://askastroraja.com',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
