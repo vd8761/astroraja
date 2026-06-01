6e4f6e1cc131580d71a36cde27b6693e466cec1f
nayira <114664058+vd8761@users.noreply.github.com>
2026-06-01 14:38:14 +0530

    Update Corrections

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(import.meta.env.DATABASE_URL || process.env.DATABASE_URL!);
 
 export default sql;
